baf44c289f53c124dfe22dd2d156957e8fb42809
Mateus Santos <santos.mateus@protonmail.com>
2023-11-05 23:56:04 -0300

    fixed route

--- app/main.py
+++ app/main.py
@@ -73,7 +73,7 @@ async def login_for_access_token(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/users/me/", response_model=specs.UserSpec)
+@app.get("/users/me", response_model=specs.UserSpec)
 async def user_read(
     current_user: Annotated[specs.UserSpec, Depends(dependecy.get_current_active_user)]
 ):
