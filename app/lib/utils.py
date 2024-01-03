a594a97460c30fb1b818c1b9a6777b06817aa31c
Mateus Santos <santos.mateus@protonmail.com>
2024-01-03 12:11:41 -0300

    created endpoint for funding task

--- app/lib/utils.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-
-def string_generator(size=32, chars=string.ascii_uppercase + string.digits):
-    return "".join(random.choice(chars) for _ in range(size))
--- app/migrations/add_task_fund_column.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS task_fund (
-    id SERIAL PRIMARY KEY,
-    amount BIGINT NOT NULL,
-    user_member_id INT REFERENCES group_membership(id) ON DELETE CASCADE,
-    task_id INT REFERENCES task(id) ON DELETE CASCADE
-);
